5298d125d398e34f01c8f947db2bb22e10c78d91
warlord200 <thedarkpaladin05@gmail.com>
2022-06-15 22:50:17 +0800

    Input console finished

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
 const numpads = document.querySelector(".numpads");
-
-// add buttons
+const currentInput = document.querySelector(".currentInput");
+const clearButton = document.querySelector(".clear");
+const deleteButton = document.querySelector(".delete");
+const operatorButton = document.querySelectorAll(".op");
+let inputArr = [];
+const compare = {
+  "+": (a, b) => a + b,
+  "-": (a, b) => a + b,
+  "*": (a, b) => a + b,
+  "/": (a, b) => a + b,
+};
+// add all buttons
 for (let i = 0; i < 10; i++) {
   const button = document.createElement("button");
   button.classList.add(`${i}`);
+  button.classList.add("num-button");
   button.innerText = `${i}`;
   numpads.appendChild(button);
 }
+const numpadButton = document.querySelectorAll(".num-button");
+
 addDot();
 addEquals();
+
+const dotButton = document.querySelector(".dot");
+const equalsButton = document.querySelector(".equals");
 
 function addDot() {
   const button = document.createElement("button");
@@ -21,4 +37,53 @@ function addEquals() {
   button.classList.add("equals");
   button.innerText = "=";
   numpads.appendChild(button);
+}
+
+// add function to buttons
+
+numpadButton.forEach((button) => {
+  button.addEventListener("click", () => {
+    inputArr.push(parseInt(button.textContent));
+    updateInputConsole();
+  });
+});
+
+operatorButton.forEach((button) => {
+  button.addEventListener("click", () => {
+    inputArr.push(button.textContent);
+    updateInputConsole();
+  });
+});
+
+clearButton.addEventListener("click", () => {
+  inputArr = [];
+  updateInputConsole();
+});
+
+deleteButton.addEventListener("click", () => {
+  inputArr.pop();
+  updateInputConsole();
+});
+
+dotButton.addEventListener("click", () => {
+  inputArr.push(".");
+  updateInputConsole();
+});
+
+equalsButton.addEventListener("click", () => {
+  calculate();
+});
+
+function updateInputConsole() {
+  currentInput.innerText = inputArr.join("");
+}
+
+// calculate function
+
+function calculate() {
+  const inputIndex = inputArr.find((input) => {
+    return typeof input === "string";
+  });
+  const splitArr = inputArr.join("").split(inputIndex);
+  const results = parseInt(splitArr[0]);
 }
